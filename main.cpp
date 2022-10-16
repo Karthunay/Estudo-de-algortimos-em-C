eec6c5ec521384b19ab58ae3ced5e4e6fb3cd941
Kellen Karthunay <83416962+Karthunay@users.noreply.github.com>
2022-10-16 14:04:15 -0300

    Aula 1

--- main.cpp
+++ main.cpp
@@ -4,70 +4,20 @@
 
 int main(int argc, char** argv) {
 	
-	float salario, inss, ir, sal_liquido;
+	float nota1;
+	float nota2;
+	float media;
 	
-	printf("Digite o seu salario bruto: R$");
-	scanf("%f", &salario);
+	printf("Insira nota 1: ");
+	scanf("%2f", &nota1);
 	
-	if(salario <= 1212 )
-	{
-		inss =salario * 0.075;
-	}
-	else
-	if(salario >= 1212.01 && salario <= 2427.35)
-	{
-		inss = salario * 0.09;
-	}
+	printf("Insira a nota 2: ");
+	scanf("%2f", &nota2);
 	
-	else
-	if(salario >= 2427.36 && salario <= 3641.03)
-	{
-		inss = salario * 0.12;
-	}
+	media = (nota1 + nota2) / 2;
 	
-	else
-	if(salario >= 3641.04 && salario <= 7087.22)
-	{
-		inss = salario * 0.14;
-	}
+	printf("media = %2f", media);
 	
 	
-		if(salario <= 1903.98 )
-	{
-		ir =salario * 1;
-	}
-	else
-	if(salario >= 1903.99 && salario <= 2826.65)
-	{
-		ir = salario * 0.075;
-	}
-	
-	else
-	if(salario >= 2826.66 && salario <= 3751.05)
-	{
-		ir = salario * 0.15;
-	}
-	
-	else
-	if(salario >= 3751.06 && salario <= 4664.68)
-	{
-		ir = salario * 0.225;
-	}
-	
-	else
-	if(salario >= 4664.69)
-	{
-		ir = salario * 0.275;
-	}	
-	
-	ir = ir-inss;
-	sal_liquido = salario-inss-ir;
-	
-	printf("\n Desconto Inss: R$%.2f\n", inss);
-	printf("\n Desconto IR: R$%.2f\n", ir);
-	
-	printf("\n Salario liquido: R$%.2f\n", sal_liquido);
-	
-		
 	return 0;
 }
